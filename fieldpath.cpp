6260aa834fb47f0f1a8c713f4edada6baeb9dcfa
Robin <me@invokr.org>
2017-02-03 18:53:35 +0100

    Copy over static fieldpath lookup

--- fieldpath.cpp
+++ fieldpath.cpp
@@ -8,76 +8,130 @@ fieldpath newFieldpath(dt* parentTbl, HuffmanTree* huf) {
     huf,
     false
   };
-  
+
+  fp.fields.reserve(256);
+  fp.index.reserve(256);
+
   fp.index.push_back(-1);
   return fp;
 }
 
-void walk(dota::bitstream &r, fieldpath* fp) {
-  int cnt = 0;
-  HuffmanTree* root = fp->tree;
-  //print_node(root);
-  HuffmanTree* node = root;
-  //print_node(node);
-  while (!fp->finished) {
-    ++cnt;
-    if (r.read(1) == 1) {
-      //std::cout << "if true\n";
-      HuffmanTree* i = (*node).right;
-      if (i->isLeaf) {
-        //std::cout << "is leaf\n";
-        node = root;
-        //std::cout << "i.Value(): " << std::to_string(i->value) << "\n";
-        //std::cout << "fieldpathOp: " << fieldpathLookup[i->value].name << "\n";
-        fieldpathLookup[i->value].fn(r, fp);
-        if (!(fp->finished)) {
-          addField(fp);
-          //std::cout << "Reached in " << std::to_string(cnt) << " bits, " << fp->fields[fp->fields.size() - 1].name << ", " << std::to_string(r.position()) << "\n";
-        }
-        
-        cnt = 0;
-      }
-      else {
-        //std::cout << "not leaf\n";
-        node = i;
-      }
+/** Static fieldpath lookup */
+inline fieldpathOp* fieldop_lookup( uint32_t id ) {
+    switch ( id ) {
+    case 0: // PlusOne
+        return &fieldpathLookup[0];
+    case 2: // FieldPathEncodeFinish
+        return &fieldpathLookup[39];
+    case 14: // PlusTwo
+        return &fieldpathLookup[1];
+    case 15: // PushOneLeftDeltaNRightNonZeroPack6Bits
+        return &fieldpathLookup[11];
+    case 24: // PushOneLeftDeltaOneRightNonZero
+        return &fieldpathLookup[8];
+    case 26: // PlusN
+        return &fieldpathLookup[4];
+    case 50: // PlusThree
+        return &fieldpathLookup[2];
+    case 51: // PopAllButOnePlusOne
+        return &fieldpathLookup[29];
+    case 217: // PushOneLeftDeltaNRightNonZero
+        return &fieldpathLookup[10];
+    case 218: // PushOneLeftDeltaOneRightZero
+        return &fieldpathLookup[7];
+    case 220: // PushOneLeftDeltaNRightZero
+        return &fieldpathLookup[9];
+    case 222: // PopAllButOnePlusNPack6Bits
+        return &fieldpathLookup[32];
+    case 223: // PlusFour
+        return &fieldpathLookup[3];
+    case 432: // PopAllButOnePlusN
+        return &fieldpathLookup[30];
+    case 438: // PushOneLeftDeltaNRightNonZeroPack8Bits
+        return &fieldpathLookup[12];
+    case 439: // NonTopoPenultimatePlusOne
+        return &fieldpathLookup[37];
+    case 442: // PopAllButOnePlusNPack3Bits
+        return &fieldpathLookup[31];
+    case 443: // PushNAndNonTopological
+        return &fieldpathLookup[26];
+    case 866: // NonTopoComplexPack4Bits
+        return &fieldpathLookup[38];
+    case 1735: // NonTopoComplex
+        return &fieldpathLookup[36];
+    case 3469: // PushOneLeftDeltaZeroRightZero
+        return &fieldpathLookup[5];
+    case 27745: // PopOnePlusOne
+        return &fieldpathLookup[27];
+    case 27749: // PushOneLeftDeltaZeroRightNonZero
+        return &fieldpathLookup[6];
+    case 55488: // PopNAndNonTopographical
+        return &fieldpathLookup[35];
+    case 55489: // PopNPlusN
+        return &fieldpathLookup[34];
+    case 55492: // PushN
+        return &fieldpathLookup[25];
+    case 55493: // PushThreePack5LeftDeltaN
+        return &fieldpathLookup[24];
+    case 55494: // PopNPlusOne
+        return &fieldpathLookup[33];
+    case 55495: // PopOnePlusN
+        return &fieldpathLookup[28];
+    case 55496: // PushTwoLeftDeltaZero
+        return &fieldpathLookup[13];
+    case 110994: // PushThreeLeftDeltaZero
+        return &fieldpathLookup[15];
+    case 110995: // PushTwoPack5LeftDeltaZero
+        return &fieldpathLookup[14];
+    case 111000: // PushTwoLeftDeltaN
+        return &fieldpathLookup[21];
+    case 111001: // PushThreePack5LeftDeltaOne
+        return &fieldpathLookup[20];
+    case 111002: // PushThreeLeftDeltaN
+        return &fieldpathLookup[23];
+    case 111003: // PushTwoPack5LeftDeltaN
+        return &fieldpathLookup[22];
+    case 111004: // PushTwoLeftDeltaOne
+        return &fieldpathLookup[17];
+    case 111005: // PushThreePack5LeftDeltaZero
+        return &fieldpathLookup[16];
+    case 111006: // PushThreeLeftDeltaOne
+        return &fieldpathLookup[19];
+    case 111007: // PushTwoPack5LeftDeltaOne
+        return &fieldpathLookup[18];
+    default:
+        return nullptr;
     }
-    else {
-      //std::cout << "if false\n";
-      HuffmanTree* i = (*node).left;
-      if (i->isLeaf) {
-        //std::cout << "is leaf\n";
-        node = root;
-        //std::cout << "i.Value(): " << std::to_string(i->value) << "\n";
-        //std::cout << "fieldpathOp: " << fieldpathLookup[i->value].name << "\n";
-        fieldpathLookup[i->value].fn(r, fp);
-        if (!(fp->finished)) {
-          addField(fp);
-          //std::cout << "Reached in " << std::to_string(cnt) << " bits, " << fp->fields[fp->fields.size() - 1].name << ", " << std::to_string(r.position()) << "\n";
-        }
-        
-        cnt = 0;
-      }
-      else {
-        //std::cout << "not leaf\n";
-        node = i;
-      }
-    }
+}
 
-  }
+void walk(dota::bitstream &r, fieldpath* fp) {
+    while (!fp->finished) {
+        int id = 0;
+        fieldpathOp* op = nullptr;
+
+        for ( uint32_t i = 0; i < 17 && !op; ++i ) {
+            id = ( id << 1 ) | r.read(1);
+            op = fieldop_lookup( id );
+        }
+
+        op->fn(r, fp);
+        if (!(fp->finished)) {
+            addField(fp);
+        }
+    }
 }
 
 void addField(fieldpath* fp) {
   dt* cDt = fp->parent;
   std::string name;
   int i;
-  
+
   /*if debugLevel >= 6 {
     std::string path;
     for (int j = 0; j < fp->index.size() - 1; ++j) {
       path += std::to_string(fp->index[i]) + "/";
     }
-    
+
     //std::cout << "Adding field with path: " << path << fp->index[fp->fields.size() - 1] << "\n";
   }*/
   //std::cout << "fp->index.size(): " << std::to_string(fp->index.size()) << "\n";
@@ -237,7 +291,7 @@ void PushNAndNonTopological(dota::bitstream &r, fieldpath* fp) {
       fp->index[i] += (int)r.nReadVarSInt32() + 1;
     }
   }
-  
+
   int count = (int)r.nReadUBitVar();
   for (int j = 0; j < count; ++j) {
     fp->index.push_back((int)r.nReadUBitVarFP());
